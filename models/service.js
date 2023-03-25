f272af7a3af2ab2f35e88fc0cd550948d43c8bf4
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-03-25 19:00:25 -0400

    created service model and modified tableName in migration and model file

---
+++ models/service.js
@@ -0,0 +1,31 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Service extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Service.init(
+    {
+      ownerId: DataTypes.INTEGER,
+      description: DataTypes.TEXT,
+      price: DataTypes.STRING,
+      location: DataTypes.INTEGER,
+      picture1: DataTypes.TEXT,
+      picture2: DataTypes.TEXT,
+      picture3: DataTypes.TEXT
+    },
+    {
+      sequelize,
+      modelName: 'Service',
+      tableName: 'services'
+    }
+  )
+  return Service
+}
