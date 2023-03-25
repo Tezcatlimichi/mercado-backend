f96eb72ba1fd87640527be562a5945fe0aa9ea83
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-03-25 18:53:36 -0400

    created produce model and modified tableName in migration and model file

---
+++ models/produce.js
@@ -0,0 +1,31 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Produce extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Produce.init(
+    {
+      ownerId: DataTypes.INTEGER,
+      description: DataTypes.TEXT,
+      picture1: DataTypes.TEXT,
+      picture2: DataTypes.TEXT,
+      picture3: DataTypes.TEXT,
+      price: DataTypes.STRING,
+      location: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Produce',
+      tableName: 'produceTable'
+    }
+  )
+  return Produce
+}
