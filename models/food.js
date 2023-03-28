e6d3089aa4d6da0c728b3931352264d799670132
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-03-27 20:48:49 -0400

    set up market associations

--- models/food.js
+++ models/food.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Food.belongsTo(moels.User, {
+      Food.belongsTo(models.User, {
         as: 'food',
         foreignKey: 'ownerId'
       })
