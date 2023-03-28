83ae7d55c1d6e7176125fc2439e9b6b1635685c1
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-03-27 20:46:49 -0400

    set up Food model associations

--- models/food.js
+++ models/food.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Food.belongsTo(moels.User, {
+        as: 'food',
+        foreignKey: 'ownerId'
+      })
     }
   }
   Food.init(
