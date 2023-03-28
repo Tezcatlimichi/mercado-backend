cb0973c5a15c26a65f2759f1cf2d74d4a94b84a8
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-03-27 20:49:56 -0400

    set up produce associations

--- models/produce.js
+++ models/produce.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Produce.belongsTo(models.User, {
+        as: 'produce',
+        foreignKey: 'ownerId'
+      })
     }
   }
   Produce.init(
