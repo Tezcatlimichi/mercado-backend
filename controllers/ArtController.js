7cd68692f75e5acd6c06b0636dc51907f3610410
Kathya Castaneda <kathyakathya.kc@gmail.com>
2023-04-04 21:14:57 -0400

    created function GetArt that reads a single art model by id

--- controllers/ArtController.js
+++ controllers/ArtController.js
@@ -1,11 +1,27 @@
 const { Art } = require('../models')
 
-//read
-const GetArt = async (req, res) => {
+//read all art
+const GetAllArt = async (req, res) => {
   try {
     const allArt = await Art.findAll()
     res.send(allArt)
   } catch (error) {
     throw error
   }
+}
+
+//read single art
+const GetArt = async (req, res) => {
+  try {
+    let artId = parseInt(req.paras.id)
+    const oneArt = await Art.findOne({ where: { id: artId } })
+    res.send(oneArt)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllArt,
+  GetArt
 }
